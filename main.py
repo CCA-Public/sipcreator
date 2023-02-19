c4dc18e74d04961913f11978fc64418889493493
Tessa Walsh <tessa@bitarchivist.net>
2023-02-19 15:59:21 -0500

    Remove Bag multithreading due to bug

--- main.py
+++ main.py
@@ -162,7 +162,10 @@ class SIPThread(QThread):
 
         # Bag files or write checksum manifest.
         if bag_files:
-            subprocess.call("bagit.py --processes 4 '{}'".format(sip_dir), shell=True)
+            # TODO: Multithread bagging via --processes when bug described at
+            # https://github.com/LibraryOfCongress/bagit-python/issues/130 is
+            # resolved.
+            subprocess.call("bagit.py '{}'".format(sip_dir), shell=True)
         else:
             md5deep_cmd = "cd '{}' && md5deep -rl ../objects > checksum.md5".format(
                 metadata_dir
